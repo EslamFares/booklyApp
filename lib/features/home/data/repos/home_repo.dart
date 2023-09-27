b1296d8afa0bff488ef936a31981f6e9e2d75306
EslamFares <eslamfareselazb@gmail.com>
2023-09-27 22:24:08 +0300

    use either pub and create failures class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,5 +1,10 @@
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+
 abstract class HomeRepo {
   // teams detect what method will do in screen (mn 3'ir m8olh)
-  fetchBestSellerBooks();
-  fetchBestFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestFeaturedBooks();
 }
